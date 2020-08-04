94c29fa61ad280935b5b8141e8699f51932628d0
Rey David <rdominguez@tecnologer.net>
2020-08-03 20:42:05 -0500

    Created IncompleteCommands

---
+++ incomplete_command.py
@@ -0,0 +1,13 @@
+
+class IncompleteCommands:
+
+    def __init__(self):
+        super().__init__()
+
+
+class IncompleteCommand:
+    command = None
+    author = None
+
+    def __init__(self):
+        super().__init__()
