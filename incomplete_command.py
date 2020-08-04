ceae104f5f9365f2c4afc48148b600a82411c385
Rey David <rdominguez@tecnologer.net>
2020-08-03 20:56:33 -0500

    IncompleteCommands: create crud functions

--- incomplete_command.py
+++ incomplete_command.py
@@ -1,13 +1,36 @@
+import hashlib
+
 
 class IncompleteCommands:
+    __commands = {}
 
     def __init__(self):
         super().__init__()
 
+    def add(self, update, command):
+        author = self.get_author(update)
+        if author in self.commands:
+            return "Please complete {0} first or user /cancel".format(self.__commands[author])
 
-class IncompleteCommand:
-    command = None
-    author = None
+        self.commands[author] = command
+        return None
 
-    def __init__(self):
-        super().__init__()
+    def mark_completed(self, update, command):
+        author = self.get_author(update)
+        if author in self.commands:
+            return "Nothing pending"
+        del self.commands[author]
+        return None
+
+    def get_author(self, update):
+        userid = update.effective_user.id
+        chatid = update.effective_chat.id
+        author = b"{0}-{1}".format(userid, chatid)
+        return hashlib.md5(str.encode(author)).hexdigest()
+
+    def user_has_incomplete_command(self, update):
+        author = self.get_author(update)
+        return author in self.__commands
+
+    def clear(self):
+        self.__commands = {}
